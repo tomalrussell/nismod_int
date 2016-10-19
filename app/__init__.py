7bccf0848ac7fb1b607c90d4c98151bac3bfa5b1
Tom Russell <tomalrussell@gmail.com>
2016-10-19 13:45:34 +0100

    Run web app in python 2/3

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,12 @@ from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, make_response, render_template, safe_join
 import json
 import os
+import sys
 
+# load environment variables
 load_dotenv(find_dotenv())
 
+PY3 = (sys.version_info[0] == 3)
 DEBUG = (os.environ.get("DEBUG") == "true")
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(SITE_ROOT, "data")
@@ -16,17 +19,25 @@ app.debug = DEBUG
 
 @app.route("/")
 def hello():
+    """Render index.html page at site root
+    """
     return render_template("index.html")
 
 @app.route("/data/<filename>")
 def serve_static_data(filename):
+    """Serve json data with correct mime type
+    """
     path = safe_join(DATA_DIR, filename)
     if not os.path.exists(path):
         data = '{"error": "Not found"}'
         code = 404
     else:
-        with open(path, 'r', encoding='utf-8') as file_handle:
-            data = file_handle.read()
+        if PY3:
+            with open(path, 'r', encoding='utf-8') as file_handle:
+                data = file_handle.read()
+        else:
+            with open(path, 'r') as file_handle:
+                data = file_handle.read()
         code = 200
 
     resp = make_response(data, code)
