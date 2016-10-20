5914f984518e83e3713b3ca6833b2b6ef351b352
Tom Russell <tomalrussell@gmail.com>
2016-10-20 11:20:44 +0100

    add reload button, comment js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,13 @@
-var APP = {
+(function(window, document, undefined){
+
+// APP global to hold application state for direct reference
+var APP = window.APP = {
     layers: {}
 }
 
-var map_locations = {
+// possible locations for map view
+// - could load from database areas table?
+APP.map_locations = {
     gaza: {
         lon: 34.4,
         lat: 31.4,
@@ -10,18 +15,19 @@ var map_locations = {
     }
 }
 
-var map = L.map('main-map').setView(map_locations.gaza, map_locations.gaza.zoom);
-
+// set up leaflet map with plain geographical base layer
+var map = L.map('main-map').setView(APP.map_locations.gaza, APP.map_locations.gaza.zoom);
 var basemap_url = "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
-
 L.tileLayer(basemap_url, {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
+// layer to hold lines (added first to draw under subsequent point layers)
 APP.layers.base_lines = L.layerGroup().addTo(map);
 
 
 function get_icon_html(type){
+    // map from feature type to fontawesome icon
     var node_icon_class_by_type = {
         "bank": 'money',
         "electricity_source": 'bolt',
@@ -52,6 +58,7 @@ function setup(data){
 function addLayer(features){
     return L.geoJSON(features, {
         pointToLayer: function (feature, latlng) {
+            // create marker for this feature with an icon
             var html = get_icon_html(feature.properties.type);
             var marker = L.marker(latlng, {
                 icon: L.divIcon({
@@ -60,13 +67,21 @@ function addLayer(features){
                 })
             });
 
+            // set up id for ease of reference later
             marker._leaflet_id = feature.properties.id;
 
+            // define click interaction, to show details on focus
             marker.on("click", function(e){
                 if(this._icon.classList.contains("icon-focus")){
+                    // remove focus from this feature
                     this._icon.classList.remove("icon-focus");
                     closeDetails(this.feature);
                 } else {
+                    // remove focus from any other features
+                    _.each(document.querySelectorAll(".icon-focus"), function(el){
+                        el.classList.remove("icon-focus");
+                    });
+                    // focus on this feature
                     this._icon.classList.add("icon-focus");
                     showDetails(this.feature);
                 }
@@ -98,6 +113,7 @@ function showDetails(feature){
 }
 
 function closeDetails(feature){
+    // clear/close details sidebar
     var mountNode = document.querySelector(".main-controls");
     mountNode.innerHTML = "";
 
@@ -105,6 +121,9 @@ function closeDetails(feature){
 }
 
 function createDetailsEl(props){
+    // template for details sidebar
+    // - could use template library (react/mustache?) if there's a need for
+    //   more complex templating and data updates
     var wrap = document.createElement("div");
     var name_key = document.createElement("h3");
     var name_value = document.createElement("p");
@@ -139,9 +158,25 @@ function createDetailsEl(props){
     return wrap;
 }
 
+function clear_cache(){
+    localStorage.clear();
+}
+
+function clear_cache_and_reload(){
+    clear_cache();
+    window.location.reload(false);
+}
+
+var reload_button = document.querySelector(".clear-cache-reload");
+if (reload_button){
+    reload_button.addEventListener("click", function(e){
+        e.preventDefault();
+        clear_cache_and_reload();
+    });
+}
 
 // assume localStorage available
-// run localStorage.clear() from console to flush
+// - could 'cut the mustard' and fall back to no js for older browsers
 var cached = localStorage.getItem('gaza_infrastructure.json')
 if(cached !== null){
     var data = JSON.parse(cached);
@@ -157,3 +192,4 @@ if(cached !== null){
     });
 }
 
+})(window, document);
