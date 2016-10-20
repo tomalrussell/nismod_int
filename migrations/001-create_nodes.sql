7d8ed80a9b23d705ef73305c899a75e0556aca00
Tom Russell <tomalrussell@gmail.com>
2016-10-20 17:16:17 +0100

    add enum for data_status, default 'staged'

--- migrations/001-create_nodes.sql
+++ migrations/001-create_nodes.sql
@@ -1,13 +1,18 @@
 -- Nodes are the infrastructure assets that make of all infrastructure networks
 --  (e.g. power generators, reservoirs, incinerators)
+
+-- enum for status of imported data
+CREATE TYPE data_status AS ENUM ('staged', 'approved', 'archived');
+
 CREATE TABLE sos_i_nodes (
     node_id serial PRIMARY KEY -- Primary key and id field for table records
     , node_name text -- Name of node asset, if more than one exists per region and network
     , type text -- Node type (e.g. INTER, PUMP, CCGT, etc.) - could normalize to (integer) foreign key to nodetype_id in sos_lu_entity_types
     , area text -- Area where node resides - could normalize to (integer) foreign key to nodetype_id in sos_lu_areas
     , function text -- Node function (source, sink, intermediate, demand)
-    , last_updated timestamp with time zone -- Date that this data was last updated
-    , data_source_id integer -- Data grouped by source, Foreign key to area_id in SoS_LU_Data_Source
+    , last_updated timestamp with time zone DEFAULT now() -- Date that this data was last updated
+    , data_source_id integer -- Data grouped by source, Foreign key to area_id in sos_lu_data_source
+    , status data_status DEFAULT 'staged' -- Data is staged on import, then manually approved and eventually archived
     , ref_key text -- Tracking code or number for connection to external data reference
     , location geography(POINT, 4326) -- Node geometry (GIS location, shape)
 );
--- migrations/002-create_data_sources.sql
+++ migrations/002-create_data_sources.sql
@@ -3,5 +3,5 @@ CREATE TABLE sos_lu_data_sources (
     data_source_id serial PRIMARY KEY -- Primary key and id field for table records
     , name text -- Short name for the data, eg central pop data
     , description text -- Detailed description of the data, eg The central population scenario data
-    , url text -- URL of the data source, eg http://download.geofabrik.de/asia/israel-and-palestine-latest.osm.pbf
+    , url text -- URL (or filename) of the data source, eg http://download.geofabrik.de/asia/israel-and-palestine-latest.osm.pbf
 );
